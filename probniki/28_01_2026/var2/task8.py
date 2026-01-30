d4a32a05a8e13dbbd45917696832a4e35383fc4e
OboznenkoAM <fapa2fa@gmail.com>
2026-01-30 11:45:54 +0300

    upd

---
+++ probniki/28_01_2026/var2/task8.py
@@ -0,0 +1,15 @@
+from itertools import product
+
+alph = 'МАНГУСТ'
+
+res = 0
+for var in product(alph, repeat=6):
+    word = ''.join(var)
+    if word[0] != 'А':
+        if word.count('У') > 0:
+            if word.count('М') == 2:
+                # print(word)
+                res += 1
+
+print(res)
+# 9155
